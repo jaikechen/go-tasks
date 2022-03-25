dcd74476aad8ebe0522c101647e17e26042fcf9a
Jack Chen <jaikechen@gmail.com>
2022-03-25 09:42:47 -0400

    saving tasks to mysql using standard library

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"errors"
 	"github.com/kataras/iris/v12"
 	"tasks/internal/handlers"
 )
 
 func main() {
 	app := iris.New()
+	app.Use(tokenAuth)
 	server := handlers.NewTaskServer()
 	app.Get("/", server.HandlerTask)
 
 	app.Listen(":8080")
 
+}
+func tokenAuth(ctx iris.Context) {
+	token := ctx.URLParam("token")
+	if token != "1234" {
+		ctx.StopWithError(403, errors.New("not authorized"))
+		return
+	}
+	ctx.Next()
 }
--- internal/taskStore/taskstore.go
+++ internal/taskStore/taskstore.go
@@ -1,6 +1,9 @@
 package taskStore
 
 import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
 	"tasks/model"
 	"time"
 )
@@ -27,14 +30,33 @@ func (ts *TaskStore) CreateTask(text string, tags []string, due time.Time) int {
 	return ts.nextId
 }
 func (ts *TaskStore) GetAllTasks() []model.Task {
-	return []model.Task{
-		{
-			Id:   1,
-			Text: "task1",
-		},
-		{
-			Id:   2,
-			Text: "task2",
-		},
+	var tasks []model.Task
+
+	db, err := sql.Open("mysql", "mailtrain:mailtrain@tcp(127.0.0.1:4306)/jackie")
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	res, err := db.Query("SELECT id,name FROM tasks")
+
+	defer res.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for res.Next() {
+
+		var task model.Task
+		err := res.Scan(&task.Id, &task.Text)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tasks = append(tasks, task)
+	}
+	return tasks
 }
